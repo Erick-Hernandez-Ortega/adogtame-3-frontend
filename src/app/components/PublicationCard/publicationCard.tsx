8726e1c95e22a343ae7933df20340c0ebbbd2f10
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-03-29 17:11:47 -0600

    feat: se añaden estilos a card component

--- src/app/components/PublicationCard/publicationCard.tsx
+++ src/app/components/PublicationCard/publicationCard.tsx
@@ -5,8 +5,8 @@ import { IconBadge, IconMessages, IconHeart } from '@tabler/icons-react';
 const PublicationCard: React.FC = () => {
   // TODO falta dehacerlo funcional
   return (
-    <div className="card text-center">
-      <div className="card-header text-start d-flex align-items-center">
+    <div className="card text-center border-0">
+      <div className="card-header text-start d-flex align-items-center bg-transparent">
         <Image src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2" />
         <strong>Toni kross</strong>
        <p className="m-0 ms-1 text-body-secondary fs-6">1 d</p>
@@ -14,14 +14,14 @@ const PublicationCard: React.FC = () => {
       <div className="card-body p-0"  >
         <Image src="https://place.dog/400/400" alt="image" width='400' height='400' />
       </div>
-      <div className="card-footer">
+      <div className="card-footer bg-transparent border-0">
         <div className="d-flex d-flex justify-content-between">
           <div className="d-flex gap-1">
-            <IconHeart size={32}/>
-            <IconMessages size={32}/>
+            <IconHeart stroke={1.5} size={32}/>
+            <IconMessages stroke={1.5} size={32}/>
           </div>
           <div>
-            <IconBadge size={32}/>
+            <IconBadge stroke={1.5}size={32}/>
           </div>
         </div>
         <div className="text-start d-flex gap-2 align-items-center mt-1">
