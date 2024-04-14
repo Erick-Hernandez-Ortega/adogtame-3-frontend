91389cb34f2632e0078c5ab1379b61c7688b4239
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-04-13 20:02:18 -0600

    perf: se cambia todo el estilo de adogtame

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@ import { Caveat } from "next/font/google";
 import { useRouter } from 'next/navigation';
 import { userLogout } from '@/utils/userAPI';
 import { AxiosError, AxiosResponse } from 'axios';
-import Sidebar from './components/Sidebar/sidebar';
-import PublicationCard from './components/PublicationCard/publicationCard';
 
 const caveat = Caveat({ subsets: ["latin"] });
 
@@ -33,12 +31,7 @@ const Home: React.FC = () => {
     }, []);
 
     return (
-        <main className='d-flex flex-fill min-vh-100' style={{maxHeight: '100vh'}}>
-            <Sidebar logout={handleLogout} isLogged={isLogged} />
-            <div className='overflow-scroll d-flex flex-column h-auto w-100 align-items-center gap-3 p-3'>
-              <PublicationCard isLogged={isLogged} />
-              <PublicationCard isLogged={isLogged} />
-            </div>
+        <main className='d-flex flex-fill min-vh-100' style={{maxHeight: '100vh'}}>\
             {/* <h1>Home component</h1>
             <button onClick={handleLogout}>Cerrar sesion</button> */}
         </main>
