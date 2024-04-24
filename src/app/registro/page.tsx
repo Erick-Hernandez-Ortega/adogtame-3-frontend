d7ca352ca0f7c108680f72656e1d0608fa6be1ea
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-04-23 20:33:22 -0600

    feat(registro): se valida el form

--- src/app/registro/page.tsx
+++ src/app/registro/page.tsx
@@ -1,6 +1,7 @@
+'use client'
 import React, { useState } from 'react';
 import Image from 'next/image';
-import { BoxStyleRegistro } from '@/types/registro';
+import { BoxStyleRegistro, FormRegisterState } from '@/types/registro';
 
 // TODO falta hacer la responsiva y funcional
 const Register: React.FC = () => {
@@ -8,6 +9,55 @@ const Register: React.FC = () => {
         boxShadow: 'rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px',
         minHeight: '90vh',
     };
+    const [formRegister, setFormRegister] = useState<FormRegisterState>({
+        name: '',
+        nameClass: '',
+        email: '',
+        emailClass: '',
+        password: '',
+        passwordClass: '',
+    });
+
+    const validateEmail = (email: string): void => {
+        const emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (emailRegex.test(email)) {
+            // Email válido
+            setFormRegister({ ...formRegister, email: email, emailClass: 'is-valid' });
+        } else {
+            // Email inválido
+            setFormRegister({ ...formRegister, email: email, emailClass: 'is-invalid' });
+        }
+    }
+
+    const validatePassword = (password: string): void => {
+        const passwordRegex: RegExp = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
+
+        if (passwordRegex.test(password)) {
+            // Password válida
+            setFormRegister({ ...formRegister, password: password, passwordClass: 'is-valid' });
+        } else {
+            // Password inválida
+            setFormRegister({ ...formRegister, password: password, passwordClass: 'is-invalid' });
+        }
+    }
+
+    const validateName = (name: string): void => {
+        const nameRegex: RegExp = /^[a-zA-Z]+$/;
+
+        if (nameRegex.test(name)) {
+            // Nombre válido
+            setFormRegister({ ...formRegister, name: name, nameClass: 'is-valid' });
+        } else {
+            // Nombre inválido
+            setFormRegister({ ...formRegister, name: name, nameClass: 'is-invalid' });
+        }
+    }
+
+
+    const isRegistrerFormValid = (): boolean => {
+        return formRegister.emailClass === 'is-valid' && formRegister.passwordClass === 'is-valid' && formRegister.nameClass === 'is-valid';
+    }
 
     return (
         <main className='d-flex justify-content-center align-items-center min-vh-100 p-3'>
@@ -19,29 +69,29 @@ const Register: React.FC = () => {
                     <Image className='d-none d-lg-block z-1' src={"/icons/registro/emojione-dog.svg"} width={150} height={150} alt='icon dog' />
                     <div className='p-3 d-flex flex-column align-items-center' style={{ width: '800px' }}>
                         <div className="form-floating mb-3 w-75 z-1">
-                            <input type="text" className={`form-control`} id="floatingInputName" placeholder="Nombre completo" />
+                            <input type="text" className={`form-control ${formRegister.nameClass}`} value={formRegister.name} onChange={(e) => validateName(e.target.value)} id="floatingInputName" placeholder="Nombre completo" />
                             <label>Nombre completo</label>
                             <div id="validationName" className="invalid-feedback">
                                 Por favor introduzca un nombre sin caracteres especiales.
                             </div>
                         </div>
                         <div className="form-floating mb-3 w-75 z-1">
-                            <input type="email" className={`form-control`} id="floatingInputEmail" placeholder="Correo electronico" />
+                            <input type="email" className={`form-control ${formRegister.emailClass}`} value={formRegister.email} onChange={(e) => validateEmail(e.target.value)} id="floatingInputEmail" placeholder="Correo electronico" />
                             <label>Correo electronico</label>
                             <div id="validationEmail" className="invalid-feedback">
                                 Por favor introduzca un correo valido.
                             </div>
                         </div>
                         <div className="form-floating mb-4 w-75 z-1">
-                            <input type="password" className={`form-control`} id="floatingInputPassword" placeholder="Contraseña" />
+                            <input type="password" className={`form-control ${formRegister.passwordClass}`} value={formRegister.password} onChange={(e) => validatePassword(e.target.value)} id="floatingInputPassword" placeholder="Contraseña" />
                             <label>Contraseña</label>
                             <div id="validationPassword" className="invalid-feedback">
-                                Por favor introduzca un correo valido.
+                                Por favor introduzca una contraseña valida.
                             </div>
                         </div>
 
                         <div className="mb-3 w-75 z-1">
-                            <button type="button" className='btn btn-primary w-100 '>Registrarse</button>
+                            <button type="button" className='btn btn-primary w-100 ' disabled={!isRegistrerFormValid()}>Registrarse</button>
                         </div>
                     </div>
                     <Image className='d-none d-lg-block z-1' src={"/icons/registro/emoji-cat.svg"} width={150} height={150} alt='icon cat' />
--- src/types/registro.ts
+++ src/types/registro.ts
@@ -2,3 +2,12 @@ export interface BoxStyleRegistro {
     boxShadow: string;
     minHeight: string;
 }
+
+export interface FormRegisterState {
+    name: string;
+    nameClass: string;
+    email: string;
+    emailClass: string;
+    password: string;
+    passwordClass: string;
+}
