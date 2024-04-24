450f62c696e98890eed57391679556c00bcd1ffe
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-04-23 20:14:29 -0600

    fix(registrar): se soluciona bug de imagen

--- src/app/registro/page.tsx
+++ src/app/registro/page.tsx
@@ -18,21 +18,21 @@ const Register: React.FC = () => {
                 <div className="d-flex flex-row justify-content-evenly align-items-center">
                     <Image src={"/icons/registro/emojione-dog.svg"} width={150} height={150} alt='icon dog' />
                     <div className='p-3 d-flex flex-column align-items-center' style={{ width: '800px' }}>
-                        <div className="form-floating mb-3 w-75 ">
+                        <div className="form-floating mb-3 w-75 z-1">
                             <input type="text" className={`form-control`} id="floatingInputName" placeholder="Nombre completo" />
                             <label>Nombre completo</label>
                             <div id="validationName" className="invalid-feedback">
                                 Por favor introduzca un nombre sin caracteres especiales.
                             </div>
                         </div>
-                        <div className="form-floating mb-3 w-75">
+                        <div className="form-floating mb-3 w-75 z-1">
                             <input type="email" className={`form-control`} id="floatingInputEmail" placeholder="Correo electronico" />
                             <label>Correo electronico</label>
                             <div id="validationEmail" className="invalid-feedback">
                                 Por favor introduzca un correo valido.
                             </div>
                         </div>
-                        <div className="form-floating mb-4 w-75">
+                        <div className="form-floating mb-4 w-75 z-1">
                             <input type="password" className={`form-control`} id="floatingInputPassword" placeholder="Contraseña" />
                             <label>Contraseña</label>
                             <div id="validationPassword" className="invalid-feedback">
@@ -40,14 +40,14 @@ const Register: React.FC = () => {
                             </div>
                         </div>
 
-                        <div className="mb-3 w-75">
+                        <div className="mb-3 w-75 z-1">
                             <button type="button" className='btn btn-primary w-100 '>Registrarse</button>
                         </div>
                     </div>
                     <Image src={"/icons/registro/emoji-cat.svg"} width={150} height={150} alt='icon cat' />
                 </div>
 
-                <div className='d-flex justify-content-center '>
+                <div className='d-flex justify-content-center z-1'>
                     <div className='d-flex justify-content-center border-top p-3 flex-column align-items-center' style={{ width: 270 }}>
                         <p className='mb-0'>¿Ya tienes una cuenta?</p>
                         <a href="" className='text-decoration-none'>Inicia Sesión</a>
