211faf128b54048791a2ed698ec8bafa5b6d4576
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-05-01 21:11:11 -0600

    feat(inicio): se hace visualmente el menu de filtros

--- src/app/components/filtrer-menu/filtrer-menu.tsx
+++ src/app/components/filtrer-menu/filtrer-menu.tsx
@@ -1,39 +1,37 @@
-import React, { FC } from 'react';
+'use client';
+import React, { FC, useState } from 'react';
 import { IconBone, IconCat, IconDog, IconGenderFemale, IconGenderMale, IconList, IconPaw } from '@tabler/icons-react';
 import { Filtrer } from '../filtrer/filtrer';
+import { Filtres } from '@/types/filtrer-menu';
 
 export const FiltrerMenu: FC = () => {
+    const [activeFilter, setActiveFilter] = useState(null as number | null);
+    const filters: Filtres[] = [
+        { icon: <IconList />, label: 'Todo' },
+        { icon: <IconDog />, label: 'Perros' },
+        { icon: <IconCat />, label: 'Gatos' },
+        { icon: <IconGenderMale />, label: 'Machos' },
+        { icon: <IconGenderFemale />, label: 'Hembras' },
+        { icon: <IconPaw />, label: 'Cachorros' },
+        { icon: <IconBone />, label: 'Adultos' },
+    ];
+
+    const handleClick = (index: number): void => {
+        setActiveFilter(index === activeFilter ? null : index);
+    };
 
     return (
-        <section className='d-flex flex-row bg-danger w-100 px-2 py-3 gap-3 '>
-            <Filtrer isActive={true}>
-                <IconList />
-                Todo
-            </Filtrer>
-            <Filtrer isActive={false}>
-                <IconDog />
-                Perros
-            </Filtrer>
-            <Filtrer isActive={false}>
-                <IconCat />
-                Gatos
-            </Filtrer>
-            <Filtrer isActive={false}>
-                <IconGenderMale />
-                Machos
-            </Filtrer>
-            <Filtrer isActive={false}>
-                <IconGenderFemale />
-                Hembras
-            </Filtrer>
-            <Filtrer isActive={false}>
-                <IconPaw />
-                Cachorros
-            </Filtrer>
-            <Filtrer isActive={false}>
-                <IconBone />
-                Adultos
-            </Filtrer>
+        <section className='d-flex flex-row w-100 px-2 py-3 gap-3 '>
+            {filters.map((filter, index: number) => (
+                <Filtrer
+                    key={index}
+                    isActive={index === activeFilter}
+                    onClick={() => handleClick(index)}
+                >
+                    {filter.icon}
+                    {filter.label}
+                </Filtrer>
+            ))}
         </section>
     );
 }
--- src/app/components/filtrer/filtrer.tsx
+++ src/app/components/filtrer/filtrer.tsx
@@ -2,14 +2,14 @@ import React, { FC } from 'react';
 
 interface FilterProps {
     children: React.ReactNode;
-    isActive: boolean
-    // Otros tipos de propiedades aquí
+    isActive: boolean,
+    onClick: () => void
 }
 
-export const Filtrer: FC<FilterProps> = ({ children, isActive }) => {
+export const Filtrer: FC<FilterProps> = ({ children, isActive, onClick }) => {
 
     return (
-        <button type="button" className='btn btn-light rounded-4 d-flex gap-1 '>
+        <button type="button" className='btn rounded-4 d-flex gap-1 btn-light' style={{ backgroundColor: isActive ? '#e1dafa' : "#f3f3f3" }} onClick={onClick}>
             {children}
         </button>
     );
