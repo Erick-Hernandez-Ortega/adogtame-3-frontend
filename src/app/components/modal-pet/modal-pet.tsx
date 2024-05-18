4ca56d777059ddcef65ec40739b4672a96833f90
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-05-18 15:33:16 -0600

    feat: se crea el modal para registrar mascotas

---
+++ src/app/components/modal-pet/modal-pet.tsx
@@ -0,0 +1,93 @@
+'use client'
+import React, { FC, useState } from 'react';
+import { Button, Group, Modal, Stepper, Switch, TextInput, Textarea } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
+import { DateInput } from '@mantine/dates';
+
+interface ModalPetProps {
+    opened: boolean;
+    close: () => void;
+}
+
+export const ModalPet: FC<ModalPetProps> = ({ opened, close }) => {
+    const isMobile: boolean | undefined = useMediaQuery('(max-width: 50em)');
+    const [active, setActive] = useState(0);
+    const nextStep = () => setActive((current) => (current < 3 ? current + 1 : current));
+    const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
+
+    return (
+        <Modal
+            opened={opened}
+            onClose={close}
+            fullScreen={isMobile}
+            size={'xl'}
+            title="Publicar una mascota en Adogtame"
+            overlayProps={{
+                backgroundOpacity: 0.55,
+                blur: 3,
+            }}
+            transitionProps={{ transition: 'fade', duration: 300 }}
+        >
+            <Stepper active={active} color='#a87feb' onStepClick={setActive}>
+                <Stepper.Step label="Primer paso" description="Información">
+                    <TextInput
+                        label="Nombre de la mascota"
+                        description="Ingresa el nombre completo de tu mascota."
+                        withAsterisk
+                        placeholder="Nombre completo"
+                        variant='filled'
+                        className='mb-3'
+                    />
+                    <TextInput
+                        label="Raza de la mascota"
+                        description="Ingresa la raza de tu mascota, si la desconoce poner 'Desconocido'."
+                        withAsterisk
+                        placeholder="Raza"
+                        className='mb-3'
+                        variant='filled'
+                    />
+                    <DateInput
+                        // value={value}
+                        // onChange={setValue}
+                        variant="filled"
+                        className='mb-3'
+                        locale='es-mx'
+                        withAsterisk
+                        valueFormat="DD MMM YYYY"
+                        description="Ingresa fecha de nacimiento de tu mascota, si la desconoce poner la fecha cuando la encontro."
+                        label="Fecha de nacimiento"
+                        placeholder="Fecha de nacimiento"
+                    />
+                    <Textarea
+                        variant="filled"
+                        label="Descripción de la mascota"
+                        className='mb-3'
+                        withAsterisk
+                        description="Proporcione detalles importantes sobre la mascota."
+                        placeholder="Es un cachorro..."
+                    />
+                    <Switch
+                        className='mb-3'
+                        label="¿Esta mascota esta esterilizada?"
+                    />
+                </Stepper.Step>
+                <Stepper.Step label="Segundo paso" description="Imagenes">
+                    Step 2 content: Verify email
+                </Stepper.Step>
+                <Stepper.Step label="Ultimo paso" description="Verificar información">
+                    Step 3 content: Get full access
+                </Stepper.Step>
+                <Stepper.Completed>
+                    Completed, click back button to get to previous step
+                </Stepper.Completed>
+            </Stepper>
+
+            <Group justify="center" mt="xl">
+                <Button variant='default' onClick={prevStep}>Atras</Button>
+                <Button onClick={nextStep}  style={{ backgroundColor: '#a87feb' }}>Siguiente</Button>
+            </Group>
+        </Modal>
+
+    );
+}
+
--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -2,6 +2,9 @@ import React, { FC } from 'react';
 import { Caveat } from "next/font/google";
 import { IconLogin, IconPlus } from '@tabler/icons-react';
 import Link from 'next/link';
+import { useDisclosure } from '@mantine/hooks';
+import { Button } from '@mantine/core';
+import { ModalPet } from '../modal-pet/modal-pet';
 
 const caveat = Caveat({ subsets: ["latin"] });
 
@@ -10,35 +13,39 @@ interface NavbarProps {
 }
 
 export const Navbar: FC<NavbarProps> = ({ token }) => {
+    const [opened, { open, close }] = useDisclosure(false);
 
     return (
-        <header className="py-3 border-bottom" style={{ backgroundColor: '#e1dafa' }}>
-            <nav className="container-fluid d-grid gap-3 align-items-center" style={{ gridTemplateColumns: '1fr 2fr' }}>
-                <div className={`${caveat.className} display-6`} style={{ color: '#35185d' }}>
-                    Adogtame
-                </div>
-
-                <div className="d-flex align-items-center">
-                    <form className="w-100 me-3 input-group " role="search">
-                        <input type="search" className="form-control rounded-4 " placeholder="Buscar..." aria-label="Search"></input>
-                    </form>
-
-                    <div className="flex-shrink-0 d-flex gap-2">
-                        {token !== null && (
-                            <button className='btn btn-primary border-0 d-flex align-items-center gap-1 ' type="button" style={{ backgroundColor: '#a87feb' }}>
-                                <IconPlus /> Publicar
-                            </button>
-                        )}
-                        {token === null && (
-                            <Link href={"/login"} className='btn btn-primary border-0 d-flex align-items-center gap-1' style={{ backgroundColor: '#474545' }}>
-                                <IconLogin /> Iniciar sesión
-                            </Link>
-                        )}
+        <>
+            <header className="py-3 border-bottom" style={{ backgroundColor: '#e1dafa' }}>
+                <nav className="container-fluid d-grid gap-3 align-items-center" style={{ gridTemplateColumns: '1fr 2fr' }}>
+                    <div className={`${caveat.className} display-6`} style={{ color: '#35185d' }}>
+                        Adogtame
                     </div>
-                </div>
-            </nav>
-        </header>
 
+                    <div className="d-flex align-items-center">
+                        <form className="w-100 me-3 input-group " role="search">
+                            <input type="search" className="form-control rounded-4 " placeholder="Buscar..." aria-label="Search"></input>
+                        </form>
+
+                        <div className="flex-shrink-0 d-flex gap-2">
+                            {token !== null && (
+                                <Button onClick={open} className='btn btn-primary border-0 d-flex align-items-center gap-1 ' style={{ backgroundColor: '#a87feb' }}>
+                                    <IconPlus />
+                                    Publicar
+                                </Button>
+                            )}
+                            {token === null && (
+                                <Link href={"/login"} className='btn btn-primary border-0 d-flex align-items-center gap-1' style={{ backgroundColor: '#474545' }}>
+                                    <IconLogin /> Iniciar sesión
+                                </Link>
+                            )}
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <ModalPet opened={opened} close={close} />
+        </>
     );
 }
 
