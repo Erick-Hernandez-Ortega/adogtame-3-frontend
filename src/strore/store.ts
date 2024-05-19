267e2b218b7dbd9e4207696f0afd2ac8d615ac0f
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-18 19:49:57 -0600

    Agregando almacenamiento persistente

--- src/strore/store.ts
+++ src/strore/store.ts
@@ -1,25 +1,27 @@
-import { create } from "zustand";
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 type StoreState = {
-    isLoading: boolean,
-    token: string,
+    isLoading: boolean;
+    token: string;
     changeLoader: (value: boolean) => void;
     addToken: (token: string) => void;
-}
+};
 
-export const useStore = create<StoreState>((set) => ({
-    isLoading: false,
-    token: '',
-
-    changeLoader: (value) => {
-        set(() => ({
-            isLoading: value
-        }))
-    },
-
-    addToken: (token) => {
-        set(() => ({
-            token: token
-        }))
-    }
-}))
+export const useStore = create<StoreState>()(
+    persist(
+        (set) => ({
+            isLoading: false,
+            token: '',
+            changeLoader: (value) => {
+                set({ isLoading: value });
+            },
+            addToken: (token) => {
+                set({ token });
+            },
+        }),
+        {
+            name: 'userInfo', // nombre para el almacenamiento persistente
+        }
+    )
+);
