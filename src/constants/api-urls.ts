f1ec3d11846e4492a0e9f5461a6f37c580dd4f18
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-05-26 14:34:18 -0600

    feat(pet-modal): se agrega pet en api urls

--- src/constants/api-urls.ts
+++ src/constants/api-urls.ts
@@ -1,4 +1,8 @@
+// * User
 export const loginAPIURL = 'http://localhost:3001/user/login';  // Post
 export const userAPIURL = 'http://localhost:3001/user/';  // GET
 export const createNewUserAPIURL = 'http://localhost:3001/user';  // Post
 export const logoutAPIURL = 'http://localhost:3001/user/logout';  // Post
+
+// * Pet
+export const petAPIURL = 'http://localhost:3001/pet';  // Post
