375f78779685f6445057a97057914db8a92cfbfa
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-06-11 21:45:35 -0600

    feat(pet): agregar el loader

--- src/app/mascota/[slug]/page.tsx
+++ src/app/mascota/[slug]/page.tsx
@@ -1,11 +1,26 @@
-
-import React, { useState } from 'react';
+'use client'
+import { Loader } from '@/app/components/loader/loader';
+import { Navbar } from '@/app/components/navbar/navbar';
+import React, { useEffect, useState } from 'react';
 
 const Pet: React.FC = () => {
-    
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        // setIsLoading(false);
+    }, []);
 
     return (
-        <p>soy mascota</p>
+        <>
+            <Navbar />
+            {isLoading ? <Loader />
+                :
+                <main>
+                    <p>soy mascota</p>
+                </main>
+
+            }
+        </>
     );
 }
 
