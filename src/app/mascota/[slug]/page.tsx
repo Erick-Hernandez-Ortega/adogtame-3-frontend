05353796979094257ac3de96ab14582da7f1ffdc
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-08-21 19:23:47 -0600

    feat: completar la pagina de mascota

--- src/app/mascota/[slug]/page.tsx
+++ src/app/mascota/[slug]/page.tsx
@@ -15,19 +15,30 @@ interface Props {
 const Pet: React.FC<Props> = ({ params }) => {
     const [isLoading, setIsLoading] = useState(true);
     const [pet, setPet] = useState<any>({});
-    const { getPetById } = useStore();
-    
+    const [owner, setOwner] = useState<any>({});
+    const { getPetById, getUserById } = useStore();
+
     useEffect(() => {
         const getPet = async (): Promise<void> => {
             const pet = await getPetById(params.slug);
-            console.log(pet);
 
             setPet(pet);
-            setIsLoading(false);
         };
 
         getPet();
     }, [params.slug, getPetById]);
+
+    useEffect(() => {
+        const getOwner = async (): Promise<void> => {
+            const user = await getUserById(pet.owner);
+
+            setOwner(user);
+            setIsLoading(false);
+        }
+
+        if (pet.owner) getOwner();
+    }, [pet, getUserById]);
+
 
     return (
         <>
@@ -36,11 +47,11 @@ const Pet: React.FC<Props> = ({ params }) => {
                 :
                 <main className='d-flex overflow-hidden'>
                     <Sidebar />
-                    <section className='d-flex bg-danger w-100 min-vh-100 p-3 gap-2'>
-                        <Image className='bg-primary d-flex flex-grow-1 rounded-3' style={{ maxWidth: '485px', maxHeight: '711px', objectFit: 'cover' }} src={pet.image} alt="pet image" width={485} height={711}  />
+                    <section className='d-flex w-100 flex-column flex-lg-row min-vh-100 p-3 gap-2'>
+                        <Image className='d-flex flex-grow-1 rounded-3 img-fluid align-self-center align-self-lg-start' style={{ objectFit: 'cover' }} src={pet.image} alt="pet image" width={483} height={711} />
 
-                        <div className='bg-warning d-flex flex-grow-1 flex-column gap-2'>
-                            <article className='px-4 py-3 rounded-4 card' style={{ backgroundColor: '#B3A5D4' }}>
+                        <div className='d-flex flex-grow-1 flex-column gap-2'>
+                            <article className='px-4 py-3 rounded-4 card mb-3' style={{ backgroundColor: '#B3A5D4' }}>
                                 <h2>{pet.name}</h2>
                                 <h4>{pet.age}, {pet.breed}, {pet.size}</h4>
 
@@ -50,8 +61,13 @@ const Pet: React.FC<Props> = ({ params }) => {
                             </article>
                             <article>
                                 <h4 className='text-center'>Información del dueño</h4>
-                                <div className='card'>
-
+                                <div className='card p-3 d-flex flex-row align-items-center gap-4 justify-content-center'>
+                                    <Image src={pet.image} className="object-fit-cover rounded-circle" alt="owner image" width={180} height={180} />
+                                    <div>
+                                        <h5>{owner.name}</h5>
+                                        <h6>{owner.username}</h6>
+                                        <p>{owner.email}</p>
+                                    </div>
                                 </div>
                             </article>
                         </div>
