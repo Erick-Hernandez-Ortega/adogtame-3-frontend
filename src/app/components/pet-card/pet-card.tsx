dbb85e6ccb471a4052af3ce9339edcf35f754b45
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-08-21 18:57:31 -0600

    feat(pet-card): agregar cursor pointer

--- src/app/components/pet-card/pet-card.tsx
+++ src/app/components/pet-card/pet-card.tsx
@@ -15,7 +15,7 @@ export const PetCard: FC<Props> = ({ pet }) => {
     }
 
     return (
-        <article className="d-flex flex-row p-3 gap-3 rounded-4 shadow-sm" style={{ maxWidth: '330px', maxHeight: '180px', backgroundColor: '#fff' }} onClick={handleClick}>
+        <article className="d-flex flex-row p-3 gap-3 rounded-4 shadow-sm" style={{ maxWidth: '330px', maxHeight: '180px', backgroundColor: '#fff', cursor: 'pointer' }} onClick={handleClick}>
             <Image src={pet.dataUrl} className="object-fit-cover rounded-4" alt="pet image" width={130} height={150} />
             <div>
                 <h4 className="m-1">{pet.name}</h4>
