2c62aaadc8e8ca3da922422cfc1bd6e38977ad56
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-09-01 16:18:30 -0600

    Feat: Se divide el componente de sidebar para que se convierta en un server component

---
+++ src/app/components/SideBarOptions/SidebarOptions.tsx
@@ -0,0 +1,55 @@
+"use client"
+import { useStore } from "@/store/store";
+import { IconHome, IconPaw, IconUser } from "@tabler/icons-react";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { LogoutButton } from "../LogoutButton/LogoutButton";
+
+export const SidebarOption = () => {
+    const { token, user } = useStore()
+    const pathname: string = usePathname();
+
+    return (
+        <>
+            <ul className="nav nav-pills flex-column mb-auto gap-2">
+                <li className="nav-item">
+                    <Link href="/" className={`nav-link ${pathname === '/' || pathname.startsWith('/mascota/') ? 'active' : ''} d-flex align-items-center gap-1 rounded-4 link-body-emphasis`}>
+                        <IconHome color="#000" />
+                        Inicio
+                    </Link>
+                </li>
+                {token !== '' && (
+                    <>
+                        <li className="nav-item">
+                            <Link href="/publicaciones" className={`nav-link ${pathname === '/publicaciones' ? 'active' : ''} link-body-emphasis rounded-4 d-flex align-items-center gap-1`}>
+                                <IconPaw />
+                                Mis publicaciones
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="/profile" className={`nav-link ${pathname === '/profile' ? 'active' : ''} link-body-emphasis rounded-4 d-flex align-items-center gap-1`}>
+                                <IconUser />
+                                Perfil
+                            </Link>
+                        </li>
+                    </>
+                )}
+            </ul>
+            {
+                token !== null && token !== '' && (
+                    <>
+                        <hr />
+                        <div className="d-flex align-items-center justify-content-between ">
+                            <div>
+                                <Image src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2" />
+                                <strong>{user?.name}</strong>
+                            </div>
+                            <LogoutButton />
+                        </div>
+                    </>
+                )
+            }
+        </>
+    )
+}
--- src/app/components/sidebar/sidebar.tsx
+++ src/app/components/sidebar/sidebar.tsx
@@ -1,62 +1,14 @@
-'use client';
 import React, { FC } from 'react';
-import { IconHome, IconLogout, IconPaw, IconUser } from '@tabler/icons-react';
-import Link from 'next/link';
 import "./styles.css";
-import Image from 'next/image';
-import { usePathname } from 'next/navigation';
-import { useStore } from '@/store/store';
+import { SidebarOption } from '../SideBarOptions/SidebarOptions';
 
-interface SidebarProps {}
+interface SidebarProps { }
 
 export const Sidebar: FC<SidebarProps> = () => {
-    const { token, userLogOut, user } = useStore()
-    const pathname: string = usePathname();
-
-    const logout = async (): Promise<void> => {
-        const response = await userLogOut(token);
-    }
-
     return (
         <aside className="d-none d-lg-flex flex-column flex-shrink-0 p-3 m-2 rounded-4" style={{ width: '280px', backgroundColor: '#DBD2EE' }}>
-            <ul className="nav nav-pills flex-column mb-auto gap-2">
-                <li className="nav-item">
-                    <Link href="/" className={`nav-link ${pathname === '/' || pathname.startsWith('/mascota/') ? 'active' : ''} d-flex align-items-center gap-1 rounded-4 link-body-emphasis`}>
-                        <IconHome color="#000" />
-                        Inicio
-                    </Link>
-                </li>
-                {token !== '' && (
-                    <>
-                        <li className="nav-item">
-                            <Link href="/publicaciones" className={`nav-link ${pathname === '/publicaciones' ? 'active' : ''} link-body-emphasis rounded-4 d-flex align-items-center gap-1`}>
-                                <IconPaw />
-                                Mis publicaciones
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link href="/profile" className={`nav-link ${pathname === '/profile' ? 'active' : ''} link-body-emphasis rounded-4 d-flex align-items-center gap-1`}>
-                                <IconUser />
-                                Perfil
-                            </Link>
-                        </li>
-                    </>
-                )}
-            </ul>
-            {token !== null && token !== '' && (
-                <>
-                    <hr />
-                    <div className="d-flex align-items-center justify-content-between ">
-                        <div>
-                            <Image src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2" />
-                            <strong>{user?.name}</strong>
-                        </div>
-                        <IconLogout style={{ cursor: 'pointer' }} onClick={logout} />
-                    </div>
-                </>
-            )}
+            <SidebarOption />
         </aside>
-
     );
 }
 
