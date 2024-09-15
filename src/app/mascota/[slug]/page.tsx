70611bd74f5e429aeedc55efd73b38c9b6e4c0df
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2024-09-14 21:04:08 -0600

    feat: cambiar estilo de pagina de la mascota

--- src/app/mascota/[slug]/page.tsx
+++ src/app/mascota/[slug]/page.tsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react';
 import { useStore } from '@/store/store';
 import { Sidebar } from '@/app/components/sidebar/sidebar';
 import Image from 'next/image';
+import { Card, Text, Badge, Button, Group, Menu, rem } from '@mantine/core';
 
 interface Props {
     params: {
@@ -16,7 +17,8 @@ const Pet: React.FC<Props> = ({ params }) => {
     const [isLoading, setIsLoading] = useState(true);
     const [pet, setPet] = useState<any>({});
     const [owner, setOwner] = useState<any>({});
-    const { getPetById, getUserById } = useStore();
+    const [isTheOwner, setIsTheOwner] = useState(false);
+    const { getPetById, getUserById, user } = useStore();
 
     useEffect(() => {
         const getPet = async (): Promise<void> => {
@@ -30,14 +32,20 @@ const Pet: React.FC<Props> = ({ params }) => {
 
     useEffect(() => {
         const getOwner = async (): Promise<void> => {
-            const user = await getUserById(pet.owner);
+            const userOwner = await getUserById(pet.owner);
 
-            setOwner(user);
+            setOwner(userOwner);
             setIsLoading(false);
         }
 
         if (pet.owner) getOwner();
     }, [pet, getUserById]);
+
+    useEffect(() => {
+        const checkOwner = (): void => { if (user?._id === owner?._id) setIsTheOwner(true); }
+
+        if (user && owner) checkOwner();
+    }, [user, owner]);
 
 
     return (
@@ -53,23 +61,46 @@ const Pet: React.FC<Props> = ({ params }) => {
                         <div className='d-flex flex-grow-1 flex-column gap-2'>
                             <article className='px-4 py-3 rounded-4 card mb-3' style={{ backgroundColor: '#B3A5D4' }}>
                                 <h2>{pet.name}</h2>
-                                <h4>{pet.age}, {pet.breed}, {pet.size}</h4>
+                                <h4 className='text-secondary'>{pet.age}, {pet.breed}, {pet.size}</h4>
 
-                                <h6 className='mt-3 lh-sm'>{pet.description}</h6>
-                                <h5>¿Esterilizado? {pet.stirilized ? 'Si' : 'No'}</h5>
-                                <h5>Sexo: {pet.sex}</h5>
+                                <h5 className='my-3 lh-sm'>{pet.description}</h5>
+                                <Group gap="xs">
+                                    <Badge color="grape">{pet.stirilized ? 'Esterilizado' : 'No esterilizado'}</Badge>
+                                    <Badge color="grape">{pet.typeOfPet}</Badge>
+                                    <Badge color="grape">{pet.sex}</Badge>
+                                </Group>
                             </article>
-                            <article>
-                                <h4 className='text-center'>Información del dueño</h4>
-                                <div className='card p-3 d-flex flex-row align-items-center gap-4 justify-content-center'>
-                                    <Image src={pet.image} className="object-fit-cover rounded-circle" alt="owner image" width={180} height={180} />
+                            <Card shadow="sm" radius="md" withBorder>
+                                <h4 className='text-center'>
+                                    <Badge color="grape" variant="light" size="xl">Información del dueño</Badge>
+                                </h4>
+                                <div className='p-3 d-flex flex-row align-items-center gap-4 justify-content-center'>
+                                    <Card.Section>
+                                        <Image src={pet.image} className="object-fit-cover rounded-circle" alt="owner image" width={180} height={180} />
+                                    </Card.Section>
                                     <div>
                                         <h5>{owner.name}</h5>
                                         <h6>{owner.username}</h6>
                                         <p>{owner.email}</p>
                                     </div>
                                 </div>
-                            </article>
+                            </Card>
+                            <Card shadow="sm" padding="lg" radius="md" withBorder>
+                                <Text fw={500}>Tu eres el dueño de esta mascota!</Text>
+
+                                <Text c="dimmed">
+                                    Administrala para poder gestionar tu mascota
+                                </Text>
+
+                                <Group justify="center">
+                                    <Button color="grape" mt="md" radius="md">
+                                        Editarla
+                                    </Button>
+                                    <Button color="red" variant="outline" mt="md" radius="md">
+                                        Eliminarla
+                                    </Button>
+                                </Group>
+                            </Card>
                         </div>
                     </section>
                 </main>
